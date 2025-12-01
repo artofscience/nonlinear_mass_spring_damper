bb57d3f048d9570261e8b4ab5e89c69f535c2e5b
koppen <stijn.koppen@gmail.com>
2025-12-01 15:54:35 +0100

    small change to momentum based example

--- momentum_based.py
+++ momentum_based.py
@@ -25,16 +25,16 @@ if __name__ == '__main__':
     m = lambda x: 1e-6 + alpha * x
     dmdt = lambda v: alpha * v
 
-    s = 10000  # speed of frequency sweep
+    s = 80000  # speed of frequency sweep
     w = lambda t: t / s  # frequency as function of time in Hertz (cycle/s)
 
-    fe = lambda t: 10 + 0.1 * np.sin(2*pi * w(t) * t)  # external harmonic force
+    fe = lambda t: 20 + 0.1 * np.sin(2*pi * w(t) * t)  # external harmonic force
     fd = lambda v: 0.01 * v
 
     msd = MSDM(m=m, dmdt=dmdt, fe=fe, fd=fd)  # setup mass-spring-damper system
 
-    time = [0.0, 2000]  # time span
-    y0 = [10.0, 0.0]  # perturb system with nonzero initial velocity
+    time = [0.0, 16000]  # time span
+    y0 = [20.0, 0.0]  # perturb system with nonzero initial velocity
 
     sol = solve_ivp(msd, time, y0, atol=1e-10, rtol=1e-10)  # solve system
     plt.plot(sol.t, sol.y[0])  # plot position vs. time
